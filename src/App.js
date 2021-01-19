7cbf3f9dde410577131a87085db4012059d51992
김다은 <gimda-eun@gimda-eun-ui-MacBookPro.local>
2021-01-19 12:04:20 +0900

    기본 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import {withTranslation} from 'react-i18next';
-import i18next from './config/lang/i18n';
+import {Text} from 'react-native';
 
 const App = () => {
-  return <Text>{i18next.t('hello')}</Text>;
+  return <Text>hello</Text>;
 };
 
-export default withTranslation()(App);
+export default App;
